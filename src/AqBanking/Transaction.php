22b1fc0d63a547adea3122a7d173552e41f6e4ee
Alexander Menk <menk@mestrona.net>
2019-10-05 10:46:07 +0200

    Revert "WIP: Make compatible with AqBanking 5.99 / aqbanking-php issue 4"
    This reverts commit a6e69e8b
    
    Switching the renderer is not needed -- there is an Export Command
    *Doh*

--- src/AqBanking/Transaction.php
+++ src/AqBanking/Transaction.php
@@ -41,7 +41,7 @@ class Transaction
         Account $remoteAccount,
         $purpose,
         \DateTime $valutaDate,
-        \DateTime $date = null,
+        \DateTime $date,
         Money $value
     )
     {
